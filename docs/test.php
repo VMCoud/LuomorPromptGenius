c86a20a9f1941d8777fd7cc2a63c7174051a95f8
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-04-09 00:37:15 +0800

    update

--- docs/test.php
+++ docs/test.php
@@ -1,12 +1,12 @@
 <?php
 function UnicodeEncode($str) {
     //split word
-    preg_match_all('/./u',$str,$matches);
+    preg_match_all('/./u', $str, $matches);
  
     $unicodeStr = "";
     foreach($matches[0] as $m) {
         //拼接
-        $unicodeStr .= "&#".base_convert(bin2hex(iconv('UTF-8',"UCS-4",$m)),16,10);
+        $unicodeStr .= "&#" . base_convert(bin2hex(iconv('UTF-8', "UCS-4", $m)), 16, 10);
     }
     return $unicodeStr;
 }
